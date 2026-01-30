bddc08a6dbc97de69043a63425ecc87bb66551ce
HNKILLNET404 <xxxpk0111@gmail.com>
2026-01-30 15:56:25 +0700

    add new message

---
+++ teemo-attacking.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int totalTime = 0;
+        int n = timeSeries.size();
+        
+        for (int i = 0; i < n - 1; ++i) {
+            totalTime += min(duration, timeSeries[i + 1] - timeSeries[i]);
+        }
+        
+        return totalTime + duration;
+    }
+};
